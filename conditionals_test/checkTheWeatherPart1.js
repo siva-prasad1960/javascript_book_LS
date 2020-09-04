4cb40aeb479b0b3ad28f5dc76d690114ec0f1723
Siva Prasad <siva.sesi.59@gmail.com>
2020-09-04 11:11:56 -0700

    adding conditionals from JS100

---
+++ conditionals_test/checkTheWeatherPart1.js
@@ -0,0 +1,9 @@
+let weather = 'Sunny';
+
+if(weather === 'Sunny') {
+  console.log("It's a beautiful day!")
+} else if (weather === 'Rainy') {
+  console.log("Grab your umbrella");
+} else {
+  console.log("Let's stay inside.")
+}
---
+++ conditionals_test/checkTheWeatherPart2.js
@@ -0,0 +1,12 @@
+let weather = 'Sunny';
+
+switch (weather) {
+  case 'Sunny':
+    console.log("It's a beautiful day!");
+    break;
+  case 'Rainy':
+    console.log("carry your umbrella");
+    break;
+  default:
+    console.log("Lets stay inside.")
+}
